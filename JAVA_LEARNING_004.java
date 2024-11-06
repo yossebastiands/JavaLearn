34fbcc5df9f092e0dd40b3c2ba7a1b4170ebb1bf
yossebastiands <61390006+yossebastiands@users.noreply.github.com>
2024-11-06 08:53:21 +0100

    Create JAVA_LEARNING_004.java

---
+++ JAVA_LEARNING_004.java
@@ -0,0 +1,25 @@
+/**
+ * Applikationsklasse um globale und lokale Variablen zu zeigen!
+ * @author Gabriele Schmidt
+ */
+class Fl { 
+	public static final float fl =  0.2;
+	public static double fl;
+	/**
+	* Die main-Methode beinhalte die lokalen Variblen und die Ausgabe
+	* @param args - wird nicht genutzt
+	*/
+	public static void main(String  args [ ]) {	
+		float fl;
+		
+
+		System.out.println("fl" + fl);
+		
+		
+		//Datentypkonvertierung float in double
+		
+		
+		//Datentypkonvertierung double in float
+		
+	}
+}
