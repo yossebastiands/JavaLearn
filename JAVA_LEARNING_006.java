ffcd1146f5e7ba5b960d1d00781a91b161b4bae0
yossebastiands <61390006+yossebastiands@users.noreply.github.com>
2024-11-06 09:21:44 +0100

    Create JAVA_LEARNING_006.java

---
+++ JAVA_LEARNING_006.java
@@ -0,0 +1,31 @@
+public class JAVA_TRAINING_05 {
+    public static void main(String[] args) {
+        
+        // CHARACTERS
+        final char MAXCHAR = Character.MAX_VALUE;
+        System.out.println((int)MAXCHAR);
+
+        final char MINCHAR = Character.MIN_VALUE;
+        System.out.println((int)MINCHAR);
+
+        int count=1;
+
+        System.out.println(count++ + ". MAXCHAR: " + (int) MAXCHAR + "\tMINCHAR: " + (int) MINCHAR); // the \t for Tab Sequence
+
+        // SHORT
+        short MAXSHORT = Short.MAX_VALUE;
+        short MINSHORT = Short.MIN_VALUE;
+
+        System.out.println((int)MAXSHORT);
+        System.out.println((int)MINSHORT);
+
+        // BYTE
+        short MAXBYTE = Byte.MAX_VALUE;
+        short MINBYTE = Byte.MIN_VALUE;
+
+        System.out.println((int)MAXBYTE);
+        System.out.println((int)MINBYTE);
+        
+
+    }
+}
